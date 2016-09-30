d9882526456c1c2aa5e2db68f447a76299fb1094
SangHLee246 <akayasha1234@yahoo.com>
2016-09-29 20:25:40 -0400

    Added More Menus
    Made the initial registration screen and then made the next preference
    screen

--- app/src/main/java/com/example/sanghunlee_desktop/amie/HomeScreen.java
+++ app/src/main/java/com/example/sanghunlee_desktop/amie/HomeScreen.java
@@ -1,7 +1,11 @@
 package com.example.sanghunlee_desktop.amie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    public void startup(View view) {
+        //Do something in response to button
+        setContentView(R.layout.startup);
+    }
+
+    public void startupsignup(View view) {
+        //Do something in response to button
+        setContentView(R.layout.searchpreferences);
     }
 }
