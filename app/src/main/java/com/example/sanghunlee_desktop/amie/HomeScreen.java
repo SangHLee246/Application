964adf637870252bba10ab2b04a65d1916a84d3d
Chris Kim <c4r1sk1m@gmail.com>
2016-10-08 20:36:57 -0400

    removed unecssary files

--- app/src/main/java/com/example/sanghunlee_desktop/amie/HomeScreen.java
+++ app/src/main/java/com/example/sanghunlee_desktop/amie/HomeScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.EditText;
 
 
@@ -39,4 +40,5 @@ public class HomeScreen extends AppCompatActivity {
         //Do something in response to button
         setContentView(R.layout.consultantidentity);
     }
+
 }
---
+++ app/src/main/java/com/example/sanghunlee_desktop/amie/SwipeDeckAdapter.java
@@ -0,0 +1,66 @@
+package com.example.sanghunlee_desktop.amie;
+import android.widget.*;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.util.*;
+import java.util.*;
+
+import com.daprlabs.aaron.swipedeck.SwipeDeck;
+import com.squareup.picasso.Picasso;
+
+import android.widget.EditText;
+
+public abstract class SwipeDeckAdapter extends BaseAdapter {
+
+    private List<String> data;
+    private Context context;
+
+    public SwipeDeckAdapter(List<String> data, Context context) {
+        this.data = data;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            // normally use a viewholder
+          //  v = inflater.inflate(R.layout.test_card2, parent, false);
+        }
+
+        ImageView imageView = (ImageView) v.findViewById(R.id.offer_image);
+        Picasso.with(context).load(R.drawable.food).fit().centerCrop().into(imageView);
+        TextView textView = (TextView) v.findViewById(R.id.sample_text);
+        String item = (String)getItem(position);
+        textView.setText(item);
+
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Layer type: ", Integer.toString(v.getLayerType()));
+                Log.i("Hardware Accel type:", Integer.toString(View.LAYER_TYPE_HARDWARE));
+            }
+        });
+        return v;
+    }
+}
